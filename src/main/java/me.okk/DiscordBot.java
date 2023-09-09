e54072829694a47ca6d6de50a2971eed28530208
josh <jwu96@students.cs.ubc.ca>
2023-09-09 14:42:19 -0700

    bot works when running main, can add maven config in the future, doesn't do much else for now

--- src/main/java/me.okk/DiscordBot.java
+++ src/main/java/me.okk/DiscordBot.java
@@ -1,9 +1,17 @@
 package me.okk;
 
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
+
+import javax.security.auth.login.LoginException;
+
 public class DiscordBot {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws LoginException {
 
-        JDA
+        JDA bot = JDABuilder.createDefault("MTE1MDE4MjYxNjc3NjA2NTE4NQ.GxbMQg.uUn-V6nvwZTzo6saR-JH17brDsEcylDnk6f0lE")
+                .setActivity(Activity.playing("VALORANT"))
+                .build();
     }
 }
