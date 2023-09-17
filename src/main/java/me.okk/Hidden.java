76c85e5b2666dbed32a0300fe5955219eb47b128
josh w <jwu96@students.ubc.cs.ca>
2023-09-17 15:18:08 -0700

    added environmental variables

--- src/main/java/me.okk/Hidden.java
+++ src/main/java/me.okk/Hidden.java
@@ -6,8 +6,8 @@ public class Hidden {
     private static Hidden hidden;
 
     private Hidden() {
-        api_key = "key";
-        bot_token = "token";
+        api_key = System.getenv("api_key");
+        bot_token = System.getenv("bot_token");
     }
 
     public static String getApi_key() {
